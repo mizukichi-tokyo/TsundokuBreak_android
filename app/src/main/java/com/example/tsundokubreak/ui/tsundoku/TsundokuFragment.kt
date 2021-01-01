bc2dfdc5a6602d0be8bf67030c0ab60db9e26d43
mizukick <>
2021-01-01 17:08:32 +0900

    DashboardをTsundokuに変更

--- app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuFragment.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tsundokubreak.ui.dashboard
+package com.example.tsundokubreak.ui.tsundoku
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tsundokubreak.R
 
-class DashboardFragment : Fragment() {
+class TsundokuFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var tsundokuViewModel: TsundokuViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        tsundokuViewModel =
+                ViewModelProvider(this).get(TsundokuViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_tsundoku, container, false)
+        val textView: TextView = root.findViewById(R.id.text_tsundoku)
+        tsundokuViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuViewModel.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.tsundokubreak.ui.dashboard
+package com.example.tsundokubreak.ui.tsundoku
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class TsundokuViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Tsundoku Fragment"
     }
     val text: LiveData<String> = _text
 }
