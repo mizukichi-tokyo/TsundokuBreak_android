cbeb7e93a427a9ae72c5ba4e346823b7ba259065
mizukick <>
2021-01-26 22:20:47 +0900

    リサイクラービューを仮実装し、共通化項目も実装

---
+++ app/src/main/java/com/example/tsundokubreak/extensions/common_recyclerView_dataBindingAdaper.kt
@@ -0,0 +1,36 @@
+package com.example.tsundokubreak.extensions
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+
+class BindingViewHolder<T : ViewDataBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
+
+
+abstract class DataBindingAdapter<T : ViewDataBinding>(val lifecycleOwner: LifecycleOwner?) :
+        RecyclerView.Adapter<BindingViewHolder<T>>() {
+    abstract fun onCreateViewDataBinding(
+            layoutInflater: LayoutInflater,
+            parent: ViewGroup,
+            viewType: Int
+    ): T
+
+    override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+    ): BindingViewHolder<T> = LayoutInflater.from(parent.context)
+            .run { onCreateViewDataBinding(this, parent, viewType) }
+            .run { BindingViewHolder(this) }
+
+    abstract fun onBindViewDataBinding(binding: T, position: Int)
+
+    override fun onBindViewHolder(holder: BindingViewHolder<T>, position: Int) {
+        holder.binding.also {
+            it.lifecycleOwner = lifecycleOwner
+            onBindViewDataBinding(it, position)
+            it.executePendingBindings()
+        }
+    }
+}
--- app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuFragment.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuFragment.kt
@@ -4,26 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.tsundokubreak.R
 import com.example.tsundokubreak.bindLifecycleOwner
 import com.example.tsundokubreak.databinding.FragmentTsundokuBinding
+import com.example.tsundokubreak.databinding.ItemRecyclerViewBinding
+import com.example.tsundokubreak.extensions.DataBindingAdapter
 
 class TsundokuFragment : Fragment() {
 
     private lateinit var tsundokuViewModel: TsundokuViewModel
+
+    val pokemonList = listOf(
+            "ピカチュウ",
+            "カイリュー",
+            "ヤドラン",
+            "ピジョン",
+            "コダック",
+            "コラッタ",
+            "ズバット",
+            "ギャロップ",
+            "サンダース",
+            "メノクラゲ",
+            "パウワウ",
+            "カラカラ",
+            "タマタマ",
+            "ガラガラ",
+            "フシギダネ"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = FragmentTsundokuBinding.inflate(inflater, container, false).bindLifecycleOwner(viewLifecycleOwner) {
+        it.recyclerView.apply {
+            adapter = BillingItemListAdapter()
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
 
+    private inner class BillingItemListAdapter :
+            DataBindingAdapter<ItemRecyclerViewBinding>(viewLifecycleOwner) {
+
+        override fun onCreateViewDataBinding(
+                layoutInflater: LayoutInflater,
+                parent: ViewGroup,
+                viewType: Int
+        ): ItemRecyclerViewBinding = ItemRecyclerViewBinding.inflate(layoutInflater, parent, false)
+
+        override fun onBindViewDataBinding(binding: ItemRecyclerViewBinding, position: Int) {
+            binding.also {
+
+                it.positionText = position.toString()
+                it.pokemonText = pokemonList[position]
             }
+        }
+
+        override fun getItemCount(): Int = pokemonList.size ?: 0
+    }
+
 }
