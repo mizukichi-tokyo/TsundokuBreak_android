cf4203dc9fcc7025e7487b1161764e340c1b74bd
mizukick <>
2021-02-07 18:43:23 +0900

    testのViewModelを作成し、実行

--- app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoViewModel.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.tsundokubreak.ui.getBookInfo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tsundokubreak.domain.entity.bookInfo.TsundokuBook
 
 class GetBookInfoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val testInfo: MutableLiveData<TsundokuBook> =
+        MutableLiveData(
+            TsundokuBook(
+                0,
+                "すごい本",
+                "窪田瑞輝",
+                100,
+                "http://books.google.com/books/content?id=RhbBoAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+            )
+        )
 }
