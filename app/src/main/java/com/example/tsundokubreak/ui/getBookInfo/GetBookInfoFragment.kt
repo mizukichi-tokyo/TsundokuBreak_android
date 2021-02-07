d4b8c229a1469848cfd60ca13eef60a827d5548b
mizukick <>
2021-02-07 15:47:41 +0900

    http通信を許可し、GoogleBooksAPIでのサムネイルを表示

--- app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoFragment.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoFragment.kt
@@ -22,7 +22,10 @@ class GetBookInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = FragmentGetBookInfoBinding.inflate(inflater, container, false)
             .bindLifecycleOwner(viewLifecycleOwner) {
-                it.imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/144px-Kotlin-logo.svg.png"
-                
+//                it.imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/144px-Kotlin-logo.svg.png"
+
+                it.imageUrl="http://books.google.com/books/content?id=RhbBoAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+
+
     }
 }
