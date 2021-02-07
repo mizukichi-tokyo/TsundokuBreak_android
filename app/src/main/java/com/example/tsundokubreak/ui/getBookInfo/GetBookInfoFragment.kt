ea7131b9f5cc4231d169cb78e9c8dcdc8fde64b2
mizukick <>
2021-02-07 15:51:51 +0900

    http通信をgoogle.booksのみに制限

--- app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoFragment.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoFragment.kt
@@ -22,10 +22,8 @@ class GetBookInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = FragmentGetBookInfoBinding.inflate(inflater, container, false)
             .bindLifecycleOwner(viewLifecycleOwner) {
-//                it.imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/144px-Kotlin-logo.svg.png"
-
                 it.imageUrl="http://books.google.com/books/content?id=RhbBoAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-
+//                it.imageUrl="http://books.google.com/books/content?id=Wx1dLwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
 
     }
 }
