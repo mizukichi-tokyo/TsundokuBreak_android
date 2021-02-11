63e906932c245bde90310d909096a416c6804522
mizukick <>
2021-02-11 22:53:25 +0900

    不要な行を削除

--- app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoViewModel.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/getBookInfo/GetBookInfoViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.tsundokubreak.ui.getBookInfo
 
-import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,7 +12,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
