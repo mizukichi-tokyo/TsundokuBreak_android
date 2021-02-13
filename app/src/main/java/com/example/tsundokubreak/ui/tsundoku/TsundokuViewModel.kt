d10d92b84e6b65cb5b519f562a57eb06fcb991f8
mizukick <>
2021-02-13 13:53:41 +0900

    TsundokuViewModelの初期化を実装

--- app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuViewModel.kt
+++ app/src/main/java/com/example/tsundokubreak/ui/tsundoku/TsundokuViewModel.kt
@@ -1,16 +1,29 @@
 package com.example.tsundokubreak.ui.tsundoku
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tsundokubreak.domain.entity.bookInfo.TsundokuBook
+import com.example.tsundokubreak.domain.repository.bookList.BookListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @HiltViewModel
-class TsundokuViewModel @Inject constructor() : ViewModel() {
+class TsundokuViewModel @Inject constructor(
+    private val bookListRepository: BookListRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Tsundoku Fragment"
+    private val _tsundokuBookList: MutableStateFlow<List<TsundokuBook>> = MutableStateFlow(emptyList())
+    val tsundokuBookList: StateFlow<List<TsundokuBook>> = _tsundokuBookList
+
+    init {
+        viewModelScope.launch {
+            bookListRepository.getAllTsundokuBook().collect {
+                _tsundokuBookList.value = it
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
